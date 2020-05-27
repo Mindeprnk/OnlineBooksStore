ba3af9a5b5100c9dd18ab35179c5af4dd3567bcf
Minde <minde@DESKTOP-PB0NTIK>
2020-05-27 16:01:02 -0400

    Add project files.

---
+++ OnlineBooksStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineBooksStore.MvcApplication" Language="C#" %>
---
+++ OnlineBooksStore/ViewModel/RandomBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineBooksStore.Models;
+
+namespace OnlineBooksStore.ViewModel
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
