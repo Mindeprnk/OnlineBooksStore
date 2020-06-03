2af3c69d3fa789f9706cdcb1994bd2d8939f3c33
Minde <minde@DESKTOP-PB0NTIK>
2020-06-03 15:23:19 -0400

    simple comment

--- OnlineBooksStore/Models/Customer.cs
+++ OnlineBooksStore/Models/Customer.cs
@@ -11,9 +11,11 @@ namespace OnlineBooksStore.Models
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
+
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
+        //MembershipType value
         public byte MembershipTypeId { get; set; }
     }
 }
